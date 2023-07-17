7f1c321e13f6cfc0c5a21adfe11dda371e176ad8
seongmun-hwang <hwang105814@gmail.com>
2023-07-18 01:17:55 +0900

    make insert.php

---
+++ user_manage_system/insert.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <h1>신규 회원 입력</h1>
+        <form method="post" action="insert_result.php">
+            아이디 : <input type="text" name="userID"><br>
+            이름 : <input type="text" name="name"><br>
+            출생년도 : <input type="text" name="birthYear"><br>
+            지역 : <input type="text" name="addr" id=""><br>
+            휴대폰 앞자리 : <input type="text" name="mobile1"><br>
+            휴대폰 뒷자리 : <input type="text" name="mobile2" id=""><br>
+            신장 : <input type="text" name="height" id=""><br>
+            <br>
+            <input type="submit" value="정보 입력">
+        </form>
+        <br><a href="main.html">첫화면</a>
+    </body>
+</html>
