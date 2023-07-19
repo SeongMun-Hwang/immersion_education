df8d1d9e8f4066267af3bf131721b2140aeb3424
seongmun-hwang <hwang105814@gmail.com>
2023-07-19 22:07:15 +0900

    lecture 6-8 complete

--- emotion_diary(react)/src/App.js
+++ emotion_diary(react)/src/App.js
@@ -1,7 +1,9 @@
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data,setData]=useState([]);
@@ -31,6 +33,27 @@ function App() {
       data.map((it)=>it.id===targetId?{...it, content:newContent}: it)
     );
   }
+
+  //api practice
+  const getData=async()=>{
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments"
+    ).then((res)=>res.json());
+
+    const initData=res.slice(0,20).map((it)=>{
+      return{
+        author : it.email,
+        content: it.body,
+        emotion : Math.floor(Math.random()*5)+1, //floor=소수점 버림
+        created_date : new Date().getTime(),
+        id : dataId.current++
+      }
+    })
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="App">
